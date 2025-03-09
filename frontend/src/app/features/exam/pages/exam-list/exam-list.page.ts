a8d81410bd2bfd93248d154b9c19a9b8d1922d2d
RekoAmine <100092798+RekoAmine@users.noreply.github.com>
2025-03-09 18:09:25 +0100

    Suppression de dépendances non utilisées

--- frontend/src/app/features/exam/pages/exam-list/exam-list.page.ts
+++ frontend/src/app/features/exam/pages/exam-list/exam-list.page.ts
@@ -1,7 +1,6 @@
 import { Component, inject, signal } from '@angular/core';
 import { ExamService } from '../../services/exam.service';
 import { CommonModule } from '@angular/common';
-import { MatTableModule } from '@angular/material/table';
 import { ShortenNamePipe } from '../../../../shared/pipes/shorten-name.pipe';
 import { ExamStatusBadgeComponent } from '../../components/exam-status-badge/exam-status-badge.component';
 import { Router } from '@angular/router';
@@ -10,7 +9,7 @@ import {ExamFormPage} from "../exam-form/exam-form.page";
 @Component({
   selector: 'app-exam-list',
   standalone: true,
-  imports: [CommonModule, MatTableModule, ShortenNamePipe, ExamStatusBadgeComponent, ExamFormPage],
+  imports: [CommonModule, ShortenNamePipe, ExamStatusBadgeComponent, ExamFormPage],
   templateUrl: './exam-list.page.html'
 })
 export class ExamListPage {
